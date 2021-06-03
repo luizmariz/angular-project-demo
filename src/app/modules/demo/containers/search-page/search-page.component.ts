a4cf9c31e298a24af07a90bba8d2bb16302c80ba
Luiz <37978726+luizmariz@users.noreply.github.com>
2021-06-03 20:22:31 -0300

    feat: implementa e integra a página de busca

--- src/app/modules/demo/containers/search-page/search-page.component.ts
+++ src/app/modules/demo/containers/search-page/search-page.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  SearchData,
+  SearchSuggestion,
+} from './../../../../models/search.model';
+import { DemoService } from './../../../../services/demo.service';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
-  selector: 'app-search-page',
+  selector: 'demo-search-page',
   templateUrl: './search-page.component.html',
-  styleUrls: ['./search-page.component.scss']
+  styleUrls: ['./search-page.component.scss'],
 })
-export class SearchPageComponent implements OnInit {
+export class SearchPageComponent implements OnInit, OnDestroy {
+  _data: SearchData[];
+  _suggestions: SearchSuggestion[];
+  _queryString: string;
 
-  constructor() { }
+  _showConfirmDialog = false;
 
-  ngOnInit(): void {
+  private subs$: Subscription[] = [];
+
+  constructor(private demoService: DemoService) {}
+
+  ngOnInit(): void {}
+
+  ngOnDestroy(): void {
+    this.subs$.forEach((sub) => {
+      sub.unsubscribe();
+    });
   }
 
+  handleSuggestionUpdate(q: string): void {
+    this.subs$.push(
+      this.demoService.getSuggestions(q).subscribe((res) => {
+        this._suggestions = res;
+      })
+    );
+  }
+
+  handleSearch(q: string): void {
+    this._suggestions = [];
+    this._queryString = q;
+
+    this.subs$.push(
+      this.demoService.getMatchs(q).subscribe((res) => {
+        this._data = res;
+      })
+    );
+  }
 }
