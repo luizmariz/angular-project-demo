3e6ccccf47f25cbc377cfb96e69cb997ae54603e
Luiz <37978726+luizmariz@users.noreply.github.com>
2021-06-03 20:21:52 -0300

    feat: implementa a barra de busca

--- src/app/modules/demo/components/searchbar/searchbar.component.ts
+++ src/app/modules/demo/components/searchbar/searchbar.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormBuilder, FormControl } from '@angular/forms';
+
+import { SearchSuggestion } from './../../../../models/search.model';
 
 @Component({
-  selector: 'app-searchbar',
+  selector: 'demo-searchbar',
   templateUrl: './searchbar.component.html',
-  styleUrls: ['./searchbar.component.scss']
+  styleUrls: ['./searchbar.component.scss'],
 })
 export class SearchbarComponent implements OnInit {
+  _inputValue: FormControl;
 
-  constructor() { }
+  @Input() suggestions: SearchSuggestion[];
 
-  ngOnInit(): void {
+  @Output() valueChanged: EventEmitter<string> = new EventEmitter();
+  @Output() searchClicked: EventEmitter<string> = new EventEmitter();
+
+  constructor(private fb: FormBuilder) {
+    this._inputValue = this.fb.control('');
   }
 
+  ngOnInit(): void {
+    this._inputValue.valueChanges.subscribe((value: string) => {
+      this.valueChanged.emit(value);
+    });
+  }
+
+  handleSearch(): void {
+    this.searchClicked.emit(this._inputValue.value);
+    this._inputValue.reset();
+  }
+
+  handleSuggestionSelect(suggestionIndex: number) {
+    this._inputValue.setValue(this.suggestions[suggestionIndex].suggestion);
+    this.handleSearch();
+  }
 }
